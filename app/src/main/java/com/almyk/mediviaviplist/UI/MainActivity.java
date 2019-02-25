64587cf53afe9e40fac5039c46b884cbfd118772
almyk <hansentommy92@gmail.com>
2019-02-25 14:55:32 +0900

    Add navigation drawer and custom toolbar

--- app/src/main/java/com/almyk/mediviaviplist/UI/MainActivity.java
+++ app/src/main/java/com/almyk/mediviaviplist/UI/MainActivity.java
@@ -1,21 +1,43 @@
 package com.almyk.mediviaviplist.UI;
 
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.almyk.mediviaviplist.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private DrawerLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PreferenceManager.setDefaultValues(this, R.xml.preferences_vip_list, false);
+
+        mDrawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         if(savedInstanceState == null) {
             VipListFragment vipListFragment = VipListFragment.newInstance();
@@ -24,5 +46,34 @@ public class MainActivity extends AppCompatActivity {
 
             fragmentTransaction.add(R.id.fragment_container, vipListFragment).commit();
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.nav_vip_list:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new VipListFragment()).commit();
+                break;
+            case R.id.nav_online_legacy:
+                break;
+            case R.id.nav_online_pendulum:
+                break;
+            case R.id.nav_online_destiny:
+                break;
+            case R.id.nav_online_prophecy:
+                break;
+        }
+        mDrawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
