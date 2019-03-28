0644214ba4887c45edf1e3e2421e51c5731f21be
almyk <hansentommy92@gmail.com>
2019-03-28 23:11:03 +0900

    Add conditional s.t only checks for bedmages if list is non-empty

--- app/src/main/java/com/almyk/mediviaviplist/Worker/BedmageWorker.java
+++ app/src/main/java/com/almyk/mediviaviplist/Worker/BedmageWorker.java
@@ -40,36 +40,38 @@ public class BedmageWorker extends Worker {
         List<BedmageEntity> bedmages;
         try {
             bedmages = mRepository.getBedmages().getValue();
-
-            for (BedmageEntity bedmage : bedmages) {
-                PlayerEntity player = mRepository.getPlayerEntityWeb(bedmage.getName());
-                if (player == null) {
-                    continue;
-                }
-                if (bedmage.isOnline() && !!player.isOnline()) { // was online and is now offline
-                    bedmage.setOnline(false);
-
-                    long logoutTime = new Date().getTime();
-                    bedmage.setLogoutTime(logoutTime);
-                    bedmage.setTimeLeft(bedmage.getTimer());
-
-                } else if (player.isOnline()) { // bedmage is online
-                    bedmage.setOnline(true);
-                    bedmage.setTimeLeft(-1);
-                } else { // bedmage is offline
-                    Date date = new Date();
-                    long time = date.getTime();
-                    long logoutTime = bedmage.getLogoutTime();
-                    long timer = bedmage.getTimer();
-                    long remainingTime = timer - (time - logoutTime);
-                    if (remainingTime > 0) {
-                        bedmage.setTimeLeft(remainingTime);
-                    } else {
-                        bedmage.setTimeLeft(0);
-                        NotificationUtils.makeBedmageNotification(bedmage.getName(), mContext);
+            if (!bedmages.isEmpty()) {
+                for (BedmageEntity bedmage : bedmages) {
+                    PlayerEntity player = mRepository.getPlayerEntityWeb(bedmage.getName());
+                    if (player == null) {
+                        continue;
                     }
+                    bedmage.setName(player.getName());
+                    if (bedmage.isOnline() && !!player.isOnline()) { // was online and is now offline
+                        bedmage.setOnline(false);
+
+                        long logoutTime = new Date().getTime();
+                        bedmage.setLogoutTime(logoutTime);
+                        bedmage.setTimeLeft(bedmage.getTimer());
+
+                    } else if (player.isOnline()) { // bedmage is online
+                        bedmage.setOnline(true);
+                        bedmage.setTimeLeft(-1);
+                    } else { // bedmage is offline
+                        Date date = new Date();
+                        long time = date.getTime();
+                        long logoutTime = bedmage.getLogoutTime();
+                        long timer = bedmage.getTimer();
+                        long remainingTime = timer - (time - logoutTime);
+                        if (remainingTime > 0) {
+                            bedmage.setTimeLeft(remainingTime);
+                        } else {
+                            bedmage.setTimeLeft(0);
+                            NotificationUtils.makeBedmageNotification(bedmage.getName(), mContext);
+                        }
+                    }
+                    mRepository.updateBedmage(bedmage);
                 }
-                mRepository.updateBedmage(bedmage);
             }
         } catch (Exception e) {
             Log.d(TAG, "Failed to update bedmages due to exception: " + e.toString());
