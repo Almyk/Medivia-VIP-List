ca41bb44bcf70132a712edcdba34edf2634a8e05
almyk <hansentommy92@gmail.com>
2019-03-28 17:13:00 +0900

    Add logic for onCreateView in BedmageFragment

--- app/src/main/java/com/almyk/mediviaviplist/UI/BedmageFragment.java
+++ app/src/main/java/com/almyk/mediviaviplist/UI/BedmageFragment.java
@@ -3,6 +3,9 @@ package com.almyk.mediviaviplist.UI;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,8 @@ import com.almyk.mediviaviplist.R;
  * A simple {@link Fragment} subclass.
  */
 public class BedmageFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private BedmageAdapter mAdapter;
 
     public BedmageFragment() {
     }
@@ -27,6 +32,12 @@ public class BedmageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_bedmage, container, false);
+
+        mRecyclerView = rootView.findViewById(R.id.rv_bedmage_list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(mRecyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        mAdapter = new BedmageAdapter(getActivity());
+        mRecyclerView.setAdapter(mAdapter);
 
         return rootView;
     }
